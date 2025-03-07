89178b664f56e3c6d123cb862c43d1d2041e3dfe
Vadim Dolguintsev <dolvadim93@gmail.com>
2025-03-07 21:41:44 +0200

    refactor visualization

--- drum_system/visualization.py
+++ drum_system/visualization.py
@@ -3,26 +3,25 @@ from drum_system import midi_extractor
 import os
 
 class Visualization:
-
     def __init__(self):
         root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         self.midi_path = os.path.join(root_dir, "data/midi")
-        self.midi = midi_extractor.Midi_extractor()
+        self.midi = midi_extractor.MidiExtractor()
         self.tabs = self.midi.get_tabs(self.midi_path)
 
-    def get_tabs(self):
+    def get_tabs(self) -> dict:
+        """Get the tabs from the MIDI extractor."""
         return self.tabs
 
-
-class note_box:
-
-    def __init__(self, curr_note):
+class NoteBox:
+    def __init__(self, curr_note: list):
         self.delta = 0
         self.done = False
-        self.notes = [38,42,47,48]
+        self.notes = [38, 42, 47, 48]
         self.curr_note = curr_note
 
-    def show(self,frame):
+    def show(self, frame: np.ndarray) -> None:
+        """Show the note box on the given frame."""
         if self.delta < 380:
             for note in self.curr_note:
                 if note == 38:
@@ -33,10 +32,10 @@ class note_box:
                     cv.rectangle(frame, (80, self.delta), (230, self.delta + 100), (255, 0, 0), 1)
                 elif note == 48:
                     cv.rectangle(frame, (410, self.delta), (560, self.delta + 100), (255, 0, 0), 1)
-
-                self.delta = self.delta + 5
+            self.delta += 5
         else:
             self.done = True
 
-    def get_done(self):
+    def get_done(self) -> bool:
+        """Check if the note box is done."""
         return self.done
