8ea9648c5e55bcf40d855b8bfa7e8948f96b947b
Vadim Dolguintsev <dolvadim93@gmail.com>
2025-03-07 21:42:02 +0200

    refactor main

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-# necessary imports
-import cv2
-import numpy as np
 import pygame
-from drum_system import app,midi_extractor,visualization
-import imutils
-from music21 import converter, corpus, instrument, midi, note, chord, pitch
+from drum_system import app
 
 # pygame init
 pygame.mixer.pre_init()
 pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
 
-# Music mp3 load
-#pygame.mixer.music.load("add path to audio")
-#pygame.mixer.music.play()
-# Importing sounds
-
 if __name__ == "__main__":
-    app = app.App()
-    app.run()
+    application = app.App()
+    application.run()
 
 
 
